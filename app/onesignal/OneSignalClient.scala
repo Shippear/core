e5e569e052a8e1a0a86b5d8d5edabaad23116747
nazarenolevy <nazareno.levy@despegar.com>
2018-06-11 10:44:02 -0300

    #9: deactivated emails if appId is not present

--- app/onesignal/OneSignalClient.scala
+++ app/onesignal/OneSignalClient.scala
@@ -16,10 +16,10 @@ case class OneSignalConfig(id: Option[String], auth: Option[String])
 class OneSignalClient @Inject()(client: WSClient)(implicit ec: ExecutionContext) extends ConfigReader with Logging with SnakeCaseJsonProtocol {
 
   val config = envConfiguration.getConfig("email-notification").as[OneSignalConfig]
+  val activated = config.id.isDefined
 
   private val ContentType = ("Content-Type", "application/json;charset=utf-8")
   private val Authorization = ("Authorization", s"Basic ${config.auth.getOrElse("")}")
-
 
   //Paths Rest API One Signal
   val OneSignalPath = "https://onesignal.com/api/v1"
@@ -39,29 +39,37 @@ class OneSignalClient @Inject()(client: WSClient)(implicit ec: ExecutionContext)
 
 
   def sendEmail(playersId: List[String], emailType: EmailType): Future[EmailResponse] = {
-    val email =  Email(config.id.getOrElse(""), "Shippear", emailBody(emailType), playersId)
+    if(activated) {
+      val email = Email(config.id.getOrElse(""), "Shippear", emailBody(emailType), playersId)
 
-    client.url(NotificationPath)
-      .withHttpHeaders(ContentType, Authorization)
-      .post(email.toJson)
-      .map(_.body.parseJsonTo[EmailResponse])
+      client.url(NotificationPath)
+        .withHttpHeaders(ContentType, Authorization)
+        .post(email.toJson)
+        .map(_.body.parseJsonTo[EmailResponse])
+    } else {
+      Future(EmailResponse("Emails Deactivated!", 0))
+    }
 
   }
 
   def device(playerOneSignalId: Option[String]): Future[List[String]] = {
-    playerOneSignalId match {
-      case Some(id) =>
-        client.url(viewDevice(id)).withHttpHeaders(ContentType, Authorization).get.map {
-          response =>
-            val res: Player = response.body.parseJsonTo[Player]
-            List(res.identifier)
-        }
-      case _ =>
-        client.url(viewDevices).withHttpHeaders(ContentType, Authorization).get.map {
-          response =>
-            val res = response.body.parseJsonTo[Device]
-            res.players.map(_.identifier)
-        }
+    if(activated){
+      playerOneSignalId match {
+        case Some(id) =>
+          client.url(viewDevice(id)).withHttpHeaders(ContentType, Authorization).get.map {
+            response =>
+              val res: Player = response.body.parseJsonTo[Player]
+              List(res.identifier)
+          }
+        case _ =>
+          client.url(viewDevices).withHttpHeaders(ContentType, Authorization).get.map {
+            response =>
+              val res = response.body.parseJsonTo[Device]
+              res.players.map(_.identifier)
+          }
+      }
+    } else {
+      Future(List())
     }
 
   }
