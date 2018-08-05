e8e13ab9fe1e2d4505868c0772c4f41187324e0c
nazarenolevy <nazareno.levy@despegar.com>
2018-08-05 19:50:18 -0300

    #73: fixes in Google Maps API

--- app/model/internal/Transport.scala
+++ app/model/internal/Transport.scala
@@ -1,17 +1,3 @@
 package model.internal
 
-import model.internal.price.enum.Size.{Value, withName}
-
 case class Transport(transportType: String, domain: Option[String], model: Option[String])
-
-object TransportType extends Enumeration {
-  type TransportType = Value
-
-  val SMALL, MEDIUM, BIG = Value
-
-  implicit def toString(size: Size): String = size.toString
-
-  implicit def toSize(size: String): Size = withName(size.toUpperCase)
-}
-
-class SizeType extends TypeRef
