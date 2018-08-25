73c4ce2c3e92e31b7b7218cfb10d2b4adc7f732a
nazarenolevy <nazareno.levy@despegar.com>
2018-08-25 00:29:52 -0300

    not sending not same carrier

--- app/onesignal/OneSignalResponse.scala
+++ app/onesignal/OneSignalResponse.scala
@@ -1,9 +1,9 @@
 package onesignal
 
-case class OneSignalResponse(id: String, recipients: Int, errors: Option[List[String]])
+case class OneSignalResponse(id: String, recipients: Int, errors: Option[Seq[String]])
 
-case class InvalidPlayerIds(id: String, recipients: Int, invalidPlayerIds: List[String])
+case class InvalidPlayerIds(id: String, recipients: Int, invalidPlayerIds: Seq[String])
 
-case class NoSubscribedPlayers(id: String, recipients: Int, errors: List[String])
+case class NoSubscribedPlayers(id: String, recipients: Int, errors: Seq[String])
 
-case class OneSignalError(errors: Option[List[String]])
+case class OneSignalError(errors: Option[Seq[String]])
