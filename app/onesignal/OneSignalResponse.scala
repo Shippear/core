1f65407afd2325f1ad1d594fbacc9ef2702fb959
nazarenolevy <nazareno.levy@despegar.com>
2018-08-25 00:04:37 -0300

    fixing onesignal responses

--- app/onesignal/OneSignalResponse.scala
+++ app/onesignal/OneSignalResponse.scala
@@ -2,5 +2,8 @@ package onesignal
 
 case class OneSignalResponse(id: String, recipients: Int, errors: Option[List[String]])
 
-case class OneSignalError(errors: Option[List[String]])
+case class InvalidPlayerIds(id: String, recipients: Int, invalidPlayerIds: List[String])
 
+case class NoSubscribedPlayers(id: String, recipients: Int, errors: List[String])
+
+case class OneSignalError(errors: Option[List[String]])
